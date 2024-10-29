79ecb11e0b55e9a9b2b95f1cdec8c64430405a99
soumyasamantaray <soumyasamantaray@gmail.com>
2024-10-29 16:10:50 +0530

    edited

--- 11loops/main.go
+++ 11loops/main.go
@@ -5,7 +5,10 @@ import "fmt"
 func main() {
 	fmt.Println("Welcome to Loops")
 
-	days := []string{"Sunday", "Monday", "Tuseday", "Wednsday", "Friday", "Saturday"}
-
+	days := []string{"Sunday", "Monday", "Tuesday", "Wednesday", "Friday", "Saturday"}
 	fmt.Println(days)
+
+	for d := 0; d < len(days); d++ {
+		fmt.Println(days[d])
+	}
 }
